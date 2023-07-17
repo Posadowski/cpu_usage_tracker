b364d6217409c4fc3dbf46f8145f6f3250028c6f
Posadowski <mateuszposa@gmail.com>
2023-07-17 23:01:18 +0200

    feat: add standardized prints

---
+++ logger.h
@@ -0,0 +1,16 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#define DEVELOP_VERSION 0
+
+#if DEVELOP_VERSION == 1
+#define LOG_DEBUG(...) printf("[DEBUG] %s\n", __VA_ARGS__);
+#define LOG_INFO(...)  printf("[INFO] %s\n", __VA_ARGS__);
+#else
+#define LOG_DEBUG(x)
+#define LOG_INFO(x)
+#endif
+#define LOG_ERROR(...) printf("[ERROR] %s\n", __VA_ARGS__);
+
+
+#endif /* LOGGER_H_ */
